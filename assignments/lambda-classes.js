97e9203d57cca4a6a2e0ce371063adad8bd93051
Chike Chris <cchike2017@gmail.com>
2019-06-13 15:24:50 -0400

    working on the prototype-refactor.js file

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,47 @@
 // CODE here for your Lambda Classes
+/*#### Person
+
+* First we need a Person class.This will be our`base-class`
+* Person receives`name` `age` `location` all as props
+* Person receives`speak` as a method.
+* This method logs out a phrase`Hello my name is Fred, I am from Bedrock` where`name` and`location` are the object's own props 
+*/
+
+class Person {
+  constructor(PersonProperty) {
+    this.name = PersonProperty.name;
+    this.age = PersonProperty.age;
+    this.location = PersonProperty.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from${this.location}`;
+  }
+}
+
+/*#### Instructor
+
+* Now that we have a Person as our base class, we'll build our Instructor class.
+* Instructor uses the same attributes that have been set up by Person
+* Instructor has the following unique props:
+ * `specialty` what the Instructor is good at i.e. 'redux'
+* `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
+* `catchPhrase` i.e. `Don't forget the homies`
+* Instructor has the following methods:
+* `demo` receives a`subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+* `grade` receives a`student` object and a`subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+  * */
+
+class Instructor extends Person {
+  constructor(IntructorProperty) {
+    super(IntructorProperty);
+    this.specialty = IntructorProperty.specialty;
+    this.favLanguage = IntructorProperty.favLanguage;
+    this.catchPhrase = IntructorProperty.catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+  grade(Student, subject) {
+    return `${Student.name} receives a perfect score on ${subject}`;
+  }
+}
